9cade1ce575a5a1a46a17803e69dfa3e14fd1e45
ankitrana-321 <ankitranar595@gmail.com>
2026-03-19 07:52:22 +0530

    add a backend functionality hotel backend complete

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const bookingController = require("../controller/bookingController");
 
+// CREATE
 router.post("/guest", bookingController.createGuest);
 router.post("/other-booking/:id", bookingController.updateOtherBooking);
 router.post("/reference/:id", bookingController.updateReference);
@@ -10,5 +11,20 @@ router.post("/company/:id", bookingController.updateCompany);
 router.post("/pax/:id", bookingController.updatePax);
 router.post("/room-tariff/:id", bookingController.updateTariff);
 router.post("/advance/:id", bookingController.updateAdvance);
+
+// GET
+router.get("/all-bookings", bookingController.getAllBookings);
+router.get("/booking/:id", bookingController.getBookingById);
+
+// UPDATE
+router.put("/booking/:id", bookingController.updateBooking);
+router.put("/full-booking/:id", bookingController.updateFullBooking);
+
+// FULL GET
+router.get("/full-booking/:id", bookingController.getFullBooking);
+
+// DELETE + REFUND
+
+router.post("/refund/:id", bookingController.refundBooking);
 
 module.exports = router;
