da0b33d35ea35dfee2a12c3ca1757df05ac1c4f7
rajawatankit <rajawatankit386@gmail.com>
2026-03-23 16:21:45 +0530

    banquetcontroller.js ,index.js banquetRoutes.js

--- routes/banquetRoutes.js
+++ routes/banquetRoutes.js
@@ -4,15 +4,27 @@ const router = express.Router();
 const {
   getBanquetDashboard,
   createBanquetBooking,
+  updateBanquetBooking,
   completeBanquetBooking,
+  cancelBanquetBooking,
+  refundBanquetBooking,
   generateBanquetBill,
+  deleteBanquetBooking,
   addBanquetHall,
+  updateBanquetHall,
+  deleteBanquetHall,
 } = require("../controller/banquetController");
 
 router.get("/", getBanquetDashboard);
 router.post("/", createBanquetBooking);
+router.put("/:id", updateBanquetBooking);
 router.put("/:id/complete", completeBanquetBooking);
+router.put("/:id/cancel", cancelBanquetBooking);
+router.put("/:id/refund", refundBanquetBooking);
 router.put("/:id/bill", generateBanquetBill);
+router.delete("/:id", deleteBanquetBooking);
 router.post("/halls", addBanquetHall);
+router.put("/halls/:id", updateBanquetHall);
+router.delete("/halls/:id", deleteBanquetHall);
 
 module.exports = router;
