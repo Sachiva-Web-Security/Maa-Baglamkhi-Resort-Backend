0fc6e906dabf9ef1a36e0b67a81ebbe340b939bf
rajawatankit <rajawatankit386@gmail.com>
2026-03-25 10:48:00 +0530

    changes in user controller, user model and user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,19 +2,24 @@ const router = require("express").Router();
 const {
   createUser,
   getUsers,
+  deleteUser,
+  updateUser,
   getMe,
   changePassword,
   updateMyAvatar,
   avatarUpload,
 } = require("../controller/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 // Only admin can create users; any authenticated user can list (admin/managers)
-router.get("/me", getMe);
-router.post("/change-password", changePassword);
-router.put("/me/avatar", avatarUpload.single("avatar"), updateMyAvatar);
+router.get("/me", authMiddleware, getMe);
+router.post("/change-password", authMiddleware, changePassword);
+router.put("/me/avatar", authMiddleware, avatarUpload.single("avatar"), updateMyAvatar);
 
-router.post("/", roleMiddleware(["admin"]), createUser);
-router.get("/", getUsers);
+router.post("/", authMiddleware, roleMiddleware(["admin"]), createUser);
+router.get("/", authMiddleware, getUsers);
+router.delete("/:id", authMiddleware, roleMiddleware(["admin"]), deleteUser);
+router.put("/:id", authMiddleware, roleMiddleware(["admin"]), updateUser);
 
 module.exports = router;
